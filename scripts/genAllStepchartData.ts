1558aacd1cb360c92fd85f08847d8924d0160bd6
zk-phi <zk-phi@users.noreply.github.com>
2023-08-10 17:08:20 +0900

    Refactoring: Move some codes

--- scripts/genAllStepchartData.ts
+++ scripts/genAllStepchartData.ts
@@ -1,5 +1,62 @@
 import * as fs from "fs";
-import { getAllStepchartData } from "../lib/getAllStepchartData";
+import * as path from "path";
+import { parseSimfile } from "../lib/parseSimfile";
+
+const ROOT = "resources/prodStepcharts";
+
+type EntireMix = Mix & {
+  simfiles: Simfile[];
+};
+
+function getFiles(...dirPath: string[]): string[] {
+  const builtPath = dirPath.reduce((building, d) => {
+    return path.join(building, d);
+  }, "");
+
+  return fs.readdirSync(builtPath);
+}
+
+function getDirectories(...dirPath: string[]): string[] {
+  const builtPath = dirPath.reduce((building, d) => {
+    return path.join(building, d);
+  }, "");
+
+  return getFiles(builtPath).filter((d) => {
+    return fs.statSync(path.join(builtPath, d)).isDirectory();
+  });
+}
+
+function getAllStepchartData(): EntireMix[] {
+  const mixDirs = getDirectories(ROOT);
+
+  return mixDirs.map((mixDir) => {
+    const mixSongDirs = getDirectories(ROOT, mixDir);
+
+    const mix = {
+      mixName: mixDir.replace(/-/g, " "),
+      mixDir,
+      songCount: mixSongDirs.length,
+    };
+
+    const simfiles = mixSongDirs.map((songDir) => {
+      try {
+        return {
+          ...parseSimfile(ROOT, mixDir, songDir),
+          mix,
+        };
+      } catch (e) {
+        throw new Error(
+          `parseStepchart failed for ${ROOT}/${mixDir}/${songDir}: ${e.message} ${e.stack}`
+        );
+      }
+    });
+
+    return {
+      ...mix,
+      simfiles,
+    };
+  });
+}
 
 fs.writeFileSync(
   `${__dirname}/../resources/stepchartData.json`,
