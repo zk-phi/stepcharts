8c6ba99b3bfd31c34a27cad4db66d2f033a06327
zk-phi <zk-phi@users.noreply.github.com>
2023-08-23 03:50:17 +0900

    Refactoring: Remove unused data

--- lib/allStepchartData.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from "fs";
-
-type MixData = Mix & { simfiles: Simfile[] };
-
-const data = fs.readFileSync("resources/stepchartData.json", "utf-8");
-const parsed = JSON.parse(data) as MixData[];
-
-export default parsed;
