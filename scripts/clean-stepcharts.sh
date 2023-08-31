b423e7a2e9ef74a4d25491313356fb2611fd93f9
zk-phi <zk-phi@users.noreply.github.com>
2023-09-01 01:40:34 +0900

    Remove banner images from stepchart dir

--- scripts/clean-stepcharts.sh
+++
@@ -1,1 +0,0 @@
-rm -f resources/stepcharts/**/*.{mp3,mp4,ogg,avi,lrc}
--- scripts/removeSpacesInDirectories.js
+++ scripts/removeSpacesInDirectories.js
@@ -1,6 +1,16 @@
 const path = require("path");
 const fs = require("fs");
 
+const REMOVE_FILE_EXTS = [
+  ".mp3",
+  ".mp4",
+  ".ogg",
+  ".avi",
+  ".lrc",
+  ".png",
+  ".jpg",
+];
+
 function removeSpaces(dirPath) {
   const stat = fs.statSync(dirPath);
 
@@ -16,7 +26,15 @@ function removeSpaces(dirPath) {
     const filenameWithoutSpaces = filename.replace(/\s/g, "-");
     const newPath = dirPath.replace(filename, filenameWithoutSpaces);
 
-    fs.renameSync(dirPath, newPath);
+    if (dirPath !== newPath) {
+      console.log(`Rename ${dirPath} -> ${newPath}`);
+      fs.renameSync(dirPath, newPath);
+    }
+  } else {
+    if (REMOVE_FILE_EXTS.findIndex((ext) => ext === path.extname(dirPath)) >= 0) {
+      console.log(`Delete ${dirPath}`);
+      fs.unlinkSync(dirPath);
+    }
   }
 }
 
