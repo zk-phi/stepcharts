e3f59376ffc7ac89bcc427b27e336703668205e6
zk-phi <zk-phi@users.noreply.github.com>
2023-09-22 18:57:21 +0900

    Fix type errors

--- lib/audioContext.ts
+++ lib/audioContext.ts
@@ -13,25 +13,33 @@ export const destinations: Record<string, GainNode | null> = {
   suppressed: null,
 };
 
+const GAIN_VALUES: [string, number][] = [
+  ["normal", 1.00],
+  ["suppressed", 0.25],
+];
+
+const AUDIO_BUFFERS: [string, string][] = [
+  ["tick", "/stepcharts/cursor12.mp3"],
+  ["stop", "/stepcharts/cursor4.mp3"],
+  ["bpm", "/stepcharts/cancel1.mp3"],
+  ["shock", "/stepcharts/cursor7.mp3"],
+  ["beat", "/stepcharts/cursor6.mp3"],
+];
+
 export const prepareAudioContext = async () => {
   if (!audioContext) {
-    audioContext = new AudioContext();
-    [["normal", 1.00], ["suppressed", 0.25]].forEach(([field, gain]) => {
-      const gainNode = new GainNode(audioContext);
+    const context = new AudioContext();
+    audioContext = context;
+    GAIN_VALUES.forEach(([field, gain]) => {
+      const gainNode = new GainNode(context);
       gainNode.gain.value = gain;
-      gainNode.connect(audioContext.destination);
+      gainNode.connect(context.destination);
       destinations[field] = gainNode;
     });
-    await Promise.all([
-      ["tick", "/stepcharts/cursor12.mp3"],
-      ["stop", "/stepcharts/cursor4.mp3"],
-      ["bpm", "/stepcharts/cancel1.mp3"],
-      ["shock", "/stepcharts/cursor7.mp3"],
-      ["beat", "/stepcharts/cursor6.mp3"],
-    ].map(async ([field, file]) => {
+    await Promise.all(AUDIO_BUFFERS.map(async ([field, file]) => {
       const data =  await fetch(file);
       const arr = await data.arrayBuffer();
-      audioBuffers[field] = await audioContext.decodeAudioData(arr);
+      audioBuffers[field] = await context.decodeAudioData(arr);
     }));
   }
   return audioContext;
