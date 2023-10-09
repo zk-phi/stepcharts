b645c5535af652a34d10bf08f4d04c664266852e
zk-phi <zk-phi@users.noreply.github.com>
2023-10-10 02:56:59 +0900

    Fix dep arrs

--- lib/hooks/usePreview.ts
+++ lib/hooks/usePreview.ts
@@ -17,7 +17,7 @@ export const usePreview = (chart?: Stepchart | null): [number, boolean, () => vo
       timelineIndex.current = i;
       return (sec - timeline[i].time) * timeline[i].bpm / 60 / 4 + timeline[i].offset;
     };
-  }, [chart]);
+  }, [chart, timelineIndex]);
 
   const play = React.useCallback(() => {
     if (chart) {
@@ -26,7 +26,7 @@ export const usePreview = (chart?: Stepchart | null): [number, boolean, () => vo
       offsetRef.current = 0;
       setPlaying(true);
     }
-  }, [chart, offsetRef, setPlaying]);
+  }, [chart, startTime, timelineIndex, offsetRef, setPlaying]);
 
   const stop = React.useCallback(() => {
     setPlaying(false);
@@ -40,7 +40,7 @@ export const usePreview = (chart?: Stepchart | null): [number, boolean, () => vo
     if (playing && secToOffset && startTime.current) {
       offsetRef.current = secToOffset(((new Date()).getTime() - startTime.current) / 1000);
     }
-  }, [secToOffset, playing, offsetRef]);
+  }, [playing, secToOffset, startTime, offsetRef]);
   useAnimationFrame(tick);
 
   return [offsetRef, playing, play, stop];
