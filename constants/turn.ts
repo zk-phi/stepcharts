c928422c4f5adb74a3cc370b609cfdbe67e7c967
zk-phi <zk-phi@users.noreply.github.com>
2023-10-18 01:25:01 +0900

    Reorder turn options

--- constants/turn.ts
+++ constants/turn.ts
@@ -1,53 +1,54 @@
+// SHUFFLE5+MIRROR = SHUFFLE6
 export const TURNS: Turn[] = [
   "OFF",
   "LEFT",
   "RIGHT",
   "MIRROR",
   "SHUFFLE1 (→↑)",
-  "SHUFFLE1+LFT",
-  "SHUFFLE1+RGT",
-  "SHUFFLE1+MIR",
   "SHUFFLE2 (↓←)",
-  "SHUFFLE2+LFT",
-  "SHUFFLE2+RGT",
-  "SHUFFLE2+MIR",
   "SHUFFLE3 (↑←)",
-  "SHUFFLE3+LFT",
-  "SHUFFLE3+RGT",
-  "SHUFFLE3+MIR",
   "SHUFFLE4 (→↑)",
-  "SHUFFLE4+LFT",
-  "SHUFFLE4+RGT",
-  "SHUFFLE4+MIR",
   "SHUFFLE5 (↑↓)",
-  "SHUFFLE5+LFT",
-  "SHUFFLE5+RGT",
-  "SHUFFLE5+MIR (→←)",
+  "SHUFFLE6 (→←)",
+  "SHUFFLE1+LEFT",
+  "SHUFFLE2+LEFT",
+  "SHUFFLE3+LEFT",
+  "SHUFFLE4+LEFT",
+  "SHUFFLE5+LEFT",
+  "SHUFFLE1+RIGHT",
+  "SHUFFLE2+RIGHT",
+  "SHUFFLE3+RIGHT",
+  "SHUFFLE4+RIGHT",
+  "SHUFFLE5+RIGHT",
+  "SHUFFLE1+MIRROR",
+  "SHUFFLE2+MIRROR",
+  "SHUFFLE3+MIRROR",
+  "SHUFFLE4+MIRROR",
 ];
 
 export const TURN_VALUES: Record<Turn, [Direction, Direction, Direction, Direction]> = {
-  "OFF":               [0, 1, 2, 3],
-  "LEFT":              [2, 0, 3, 1],
-  "RIGHT":             [1, 3, 0, 2],
-  "MIRROR":            [3, 2, 1, 0],
-  "SHUFFLE1 (→↑)":     [0, 1, 3, 2], // 2-3
-  "SHUFFLE1+LFT":      [3, 0, 2, 1],
-  "SHUFFLE1+RGT":      [1, 2, 0, 3],
-  "SHUFFLE1+MIR":      [2, 3, 1, 0],
-  "SHUFFLE2 (↓←)":     [1, 0, 2, 3], // 0-1
-  "SHUFFLE2+LFT":      [2, 1, 3, 0],
-  "SHUFFLE2+RGT":      [0, 3, 1, 2],
-  "SHUFFLE2+MIR":      [3, 2, 0, 1],
-  "SHUFFLE3 (↑←)":     [2, 1, 0, 3], // 0-2
-  "SHUFFLE3+LFT":      [0, 2, 3, 1],
-  "SHUFFLE3+RGT":      [1, 3, 2, 0],
-  "SHUFFLE3+MIR":      [3, 0, 1, 2],
-  "SHUFFLE4 (→↑)":     [0, 3, 2, 1], // 1-3
-  "SHUFFLE4+LFT":      [2, 0, 1, 3],
-  "SHUFFLE4+RGT":      [3, 1, 0, 2],
-  "SHUFFLE4+MIR":      [1, 2, 3, 0],
-  "SHUFFLE5 (↑↓)":     [0, 2, 1, 3], // 1-2
-  "SHUFFLE5+LFT":      [1, 0, 3, 2],
-  "SHUFFLE5+RGT":      [2, 3, 0, 1],
-  "SHUFFLE5+MIR (→←)": [3, 1, 2, 0], // 0-3
+  "OFF":             [0, 1, 2, 3],
+  "LEFT":            [2, 0, 3, 1],
+  "RIGHT":           [1, 3, 0, 2],
+  "MIRROR":          [3, 2, 1, 0],
+  "SHUFFLE1 (→↑)":   [0, 1, 3, 2], // 2-3
+  "SHUFFLE2 (↓←)":   [1, 0, 2, 3], // 0-1
+  "SHUFFLE3 (↑←)":   [2, 1, 0, 3], // 0-2
+  "SHUFFLE4 (→↑)":   [0, 3, 2, 1], // 1-3
+  "SHUFFLE5 (↑↓)":   [0, 2, 1, 3], // 1-2
+  "SHUFFLE6 (→←)":   [3, 1, 2, 0], // 0-3
+  "SHUFFLE1+LEFT":   [3, 0, 2, 1],
+  "SHUFFLE2+LEFT":   [2, 1, 3, 0],
+  "SHUFFLE3+LEFT":   [0, 2, 3, 1],
+  "SHUFFLE4+LEFT":   [2, 0, 1, 3],
+  "SHUFFLE5+LEFT":   [1, 0, 3, 2],
+  "SHUFFLE1+RIGHT":  [1, 2, 0, 3],
+  "SHUFFLE2+RIGHT":  [0, 3, 1, 2],
+  "SHUFFLE3+RIGHT":  [1, 3, 2, 0],
+  "SHUFFLE4+RIGHT":  [3, 1, 0, 2],
+  "SHUFFLE5+RIGHT":  [2, 3, 0, 1],
+  "SHUFFLE1+MIRROR": [2, 3, 1, 0],
+  "SHUFFLE2+MIRROR": [3, 2, 0, 1],
+  "SHUFFLE3+MIRROR": [3, 0, 1, 2],
+  "SHUFFLE4+MIRROR": [1, 2, 3, 0],
 };
