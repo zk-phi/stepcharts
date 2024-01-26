982f3779562795ffbe17067d08e6135b31144c3d
zk-phi <zk-phi@users.noreply.github.com>
2024-01-27 03:54:57 +0900

    Move some type-defs

--- simfile.d.ts
+++ simfile.d.ts
@@ -3,6 +3,48 @@ type StepchartType = {
   feet: number;
 };
 
+type Stats = {
+  jumps: number;
+  jacks: number;
+  freezes: number;
+  gallops: number;
+  shocks: number;
+  sixteenths: number;
+  trips: number;
+};
+
+type Arrow = {
+  // other beats such as 5ths and 32nds end up being colored
+  // the same as 6ths. This probably should be "color" not "beat" TODO
+  beat: 4 | 6 | 8 | 12 | 16;
+  direction: `${0 | 1 | 2}${0 | 1 | 2}${0 | 1 | 2}${0 | 1 | 2}` | 'MMMM';
+  offset: number;
+};
+
+type FreezeBody = {
+  direction: 0 | 1 | 2 | 3;
+  startOffset: number;
+  endOffset: number;
+};
+
+type Bpm = {
+  startOffset: number;
+  endOffset: number | null;
+  bpm: number;
+};
+
+type Stop = {
+  offset: number;
+  duration: number;
+};
+
+type Stepchart = {
+  arrows: Arrow[];
+  freezes: FreezeBody[];
+  bpm: Bpm[];
+  stops: Stop[];
+};
+
 type Simfile = {
   title: Title;
   artist: string;
