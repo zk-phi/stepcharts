4bb5092c934e0581a8b20acee699544acf66e5bb
zk-phi <zk-phi@users.noreply.github.com>
2024-02-05 12:35:00 +0900

    Stop using image optimizer

--- next.config.js
+++ next.config.js
@@ -1,24 +1,11 @@
-const withPlugins = require("next-compose-plugins");
-const withOptimizedImages = require("next-optimized-images");
 const withOptimizedClassnames = require("next-optimized-classnames");
 
-module.exports = withPlugins(
-  [
-    withOptimizedClassnames,
-    [
-      withOptimizedImages,
-      {
-        inlineImageLimit: -1,
-      },
-    ],
-  ],
-  {
-    pageExtensions: ["tsx"],
-    basePath: "/stepcharts",
-    trailingSlash: true,
-    serverRuntimeConfig: {
-      PROJECT_ROOT: __dirname,
-      ROOT_DOMAIN: "ddr.stepcharts.com",
-    },
-  }
-);
+module.exports = withOptimizedClassnames({
+  pageExtensions: ["tsx"],
+  basePath: "/stepcharts",
+  trailingSlash: true,
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname,
+    ROOT_DOMAIN: "ddr.stepcharts.com",
+  },
+});
