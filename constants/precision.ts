f0c477c35f3b12dc6abd564790490d1eee918ef8
zk-phi <zk-phi@users.noreply.github.com>
2024-02-14 03:54:48 +0900

    Add comment

--- constants/precision.ts
+++ constants/precision.ts
@@ -1,7 +1,7 @@
 import Fraction from "fraction.js";
 
 export const BPM_PRECISION = 2; // 0.5
-export const OFFSET_PRECISION = 192; // max precision of dwi DWI notes
+export const OFFSET_PRECISION = 192; // max precision of dwi DWI notes (lcm of 48 and 64)
 
 export const bpmFrac = (value: number) => (
   new Fraction(Math.round(value * BPM_PRECISION), BPM_PRECISION)
