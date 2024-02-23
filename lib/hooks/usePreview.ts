6e13b377f7473ce19a421804d49a3068daeb8f44
zk-phi <zk-phi@users.noreply.github.com>
2024-02-23 20:38:19 +0900

    fix

--- lib/hooks/usePreview.ts
+++ lib/hooks/usePreview.ts
@@ -66,7 +66,7 @@ export const usePreview = (chart?: AnalyzedStepchart<number> | null): [
   }, [chart, stop]);
 
   useAnimationFrame(() => {
-    if (playing && secToOffset && startRealTime.current && startTimePos.current !== null) {
+    if (playing && secToOffset && startRealTime.current && startTimePos.current != null) {
       const elapsedTime = ((new Date()).getTime() - startRealTime.current) / 1000;
       timeRef.current = elapsedTime + startTimePos.current;
       offsetRef.current = secToOffset(timeRef.current);
