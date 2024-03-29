f05dc43e345d5ba54e5dcdb8815834b550d78452
zk-phi <zk-phi@users.noreply.github.com>
2024-03-29 20:51:12 +0900

    Refactoring: Move CompactMixCard into index.tsx

--- components/pages/index.tsx
+++ components/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import Link from "next/link";
 import { Root } from "../layout/Root";
-import { CompactMixCard } from "../CompactMixCard";
 
 import styles from "./index.module.css";
 
@@ -8,12 +8,30 @@ type IndexPageProps = {
   mixes: MixMeta[];
 };
 
+function pluralize(str: string, count: number): string {
+  if (count === 1) {
+    return str;
+  }
+  return str + "s";
+}
+
 function IndexPage({ mixes }: IndexPageProps) {
   return (
     <Root title="Stepcharts" metaDescription="DDR Stepcharts">
       <h2 className={styles.h2}>Browse by versions:</h2>
       <ul className={styles.grid}>
-        { mixes.map((mix) => <CompactMixCard key={mix.mixId} mix={mix} />) }
+        {mixes.map((mix) => (
+          <Link href={`/${mix.mixId}`}>
+            <a className={styles.card}>
+              <div>
+                <b>{mix.name}</b> <small>{mix.year}</small>
+              </div>
+              <div className={styles.songCount}>
+                <small>{mix.songs} {pluralize("song", mix.songs)}</small>
+              </div>
+            </a>
+          </Link>
+        ))}
       </ul>
     </Root>
   );
